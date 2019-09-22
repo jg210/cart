918c46191acb3cbec421714e729be9bad2d5cb5b
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-09-22 12:56:58 +0100

    Consistent use of quoting in eslintrc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports =  {
   parserOptions:  {
     ecmaVersion:  2018,
     sourceType:  'module',
-    "project": "./tsconfig.json"
+    project: "./tsconfig.json"
   },
   rules:  {
   }
